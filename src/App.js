a26ba366d193c6087ab518a14a8723d32d5864e0
Rajat Verma <rajatvermag@gmail.com>
2019-11-15 02:32:50 +0530

    Designed the front end by adding several other components. Used Particle JS library for background animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation.jsx';
-import Logo from './Components/Logo/Logo.js';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.jsx';
+import Rank from './Components/Rank/Rank.jsx';
+import Particles from 'react-particles-js';
+
+
+const particlesOptions = {
+particles: {
+    number: 30,
+    density: {
+      enable: true,
+      value_area: 800
+    }
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+                  <Particles  className="particles"
+                params={{particlesOptions}} />
         <Navigation />
-        <Logo />
-        {/* 
+        <Rank />
         <ImageLinkForm />
+        {/* 
         <FaceRecognition />  */}
       </div>
     )
